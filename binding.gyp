c1c09839215144c68f23305fa6a18eab64ff1b6f
gorgueugr <gorgue@correo.ugr.es>
2024-12-30 19:57:56 +0100

    Update binding configuration for CUPS and enhance default printer retrieval

--- binding.gyp
+++ binding.gyp
@@ -24,8 +24,26 @@
                 [
                     'OS=="linux"',
                     {
-                        "libraries": ["-lcups", "-lcupsppdc"],
+                        "include_dirs": ["/usr/include", "/usr/include/cups"],
+                        "libraries": [
+                            "-lcups",
+                        ],
                         "sources/": [["exclude", "src/win/WinPrinterManager.cpp"]],
+                    },
+                ],
+                [
+                    'OS!="win"',
+                    {
+                        "cflags": ["<!(cups-config --cflags)"],
+                        "ldflags": [
+                            "<!(cups-config --libs)"
+                            #'-lcups -lgssapi_krb5 -lkrb5 -lk5crypto -lcom_err -lz -lpthread -lm -lcrypt -lz'
+                        ],
+                        "libraries": [
+                            "<!(cups-config --libs)"
+                            #'-lcups -lgssapi_krb5 -lkrb5 -lk5crypto -lcom_err -lz -lpthread -lm -lcrypt -lz'
+                        ],
+                        "link_settings": {"libraries": ["<!(cups-config --libs)"]},
                     },
                 ],
             ],
--- index.js
+++ index.js
@@ -7,35 +7,35 @@ const defaultPrinterName = printer.getDefaultPrinterName();
 
 console.log(defaultPrinterName);
 
-console.log(printer.getPrinter(defaultPrinterName));
+// console.log(printer.getPrinter(defaultPrinterName));
 
-console.log(printer.getPrinters());
+// console.log(printer.getPrinters());
 
+// // console.log(printer.getSupportedJobCommands());
+
+// // const printerName = "Microsoft Print to PDF";
+// // const printerName = "WF-7830 Series(Red)";
+// // // const printerName = "Microsoft XPS Document Writer";
+
+// // console.log(printer.getPrinter(printerName));
+
+// // const testPrint = fs.readFileSync('test.pcl');
+// // const testPrint = "Hola Mundo".repeat(100);
+
+// // console.log(testPrint);
+
+// // const result = printer.printDirect(testPrint, printerName, 'test.PCL', 'RAW');
+// // console.log(result);
+
+// for (const print of printer.getPrinters()) {
+
+//     console.log(printer.getPrinterDevMode(print.name));
+
+
+// }
+
+
+// console.log(printer.getSupportedPrintFormats());
 // console.log(printer.getSupportedJobCommands());
-
-// const printerName = "Microsoft Print to PDF";
-// const printerName = "WF-7830 Series(Red)";
-// // const printerName = "Microsoft XPS Document Writer";
-
-// console.log(printer.getPrinter(printerName));
-
-// const testPrint = fs.readFileSync('test.pcl');
-// const testPrint = "Hola Mundo".repeat(100);
-
-// console.log(testPrint);
-
-// const result = printer.printDirect(testPrint, printerName, 'test.PCL', 'RAW');
-// console.log(result);
-
-for (const print of printer.getPrinters()) {
-
-    console.log(printer.getPrinterDevMode(print.name));
-
-
-}
-
-
-console.log(printer.getSupportedPrintFormats());
-console.log(printer.getSupportedJobCommands());
 
 // console.log(printer.getJob(defaultPrinterName, 1));
